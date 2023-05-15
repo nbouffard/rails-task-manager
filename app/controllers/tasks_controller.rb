98c1c2240e64eb7acb9b332441c7d6dd0dae71dc
nicola bouffard <nicola.bouffard855@gmail.com>
2023-05-15 16:52:44 +0200

    add destroy and link pages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
-    @first = Task.first
   end
 
   def show
@@ -23,10 +22,15 @@ class TasksController < ApplicationController
   end
 
   def update
-    # raise
     @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
   get 'tasks/:id', to: 'tasks#show', as: :task
-  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
 
 end
