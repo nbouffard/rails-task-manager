68d36cecea9dcc111c56987b71cbc87c6bffb8e3
nicola bouffard <nicola.bouffard855@gmail.com>
2023-05-15 15:11:31 +0200

    add show to route, controller and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,10 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
+    @first = Task.first
+  end
+
+  def show
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'tasks#index'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
