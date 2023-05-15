76a4704b2c69d0e074393b02147748b79c2660b0
nicola bouffard <nicola.bouffard855@gmail.com>
2023-05-15 16:57:04 +0200

    add links between pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'tasks', to: 'tasks#index'
+  get root 'tasks#index'
   get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
   get 'tasks/:id', to: 'tasks#show', as: :task
